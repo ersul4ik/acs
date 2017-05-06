1f25a33dc00fa85dc6c31cfec67c7270df5e4c01
Alexander Astashov <alexander.astashov.vl@gmail.com>
2017-05-06 21:42:37 +0600

    init reports app

---
+++ applications/reports/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf.urls import url
+
+from applications.reports.views import report_main
+
+urlpatterns = [
+    url(r'$', report_main),
+]

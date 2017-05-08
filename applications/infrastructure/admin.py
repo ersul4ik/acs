39d320627b187486ed50f9f082678337e3f488ac
Alexander Astashov <alexander.astashov.vl@gmail.com>
2017-05-08 13:16:40 +0600

    register infrastructure model and fixed profile method

--- applications/infrastructure/admin.py
+++ applications/infrastructure/admin.py
@@ -1,3 +1,9 @@
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
-# Register your models here.
+from applications.infrastructure.models import Departament
+from applications.infrastructure.models import Position
+
+admin.site.register(Departament)
+admin.site.register(Position)

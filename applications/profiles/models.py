6ca50f27c1be75c1d4930050164d2e367ed6bf59
ersul4ik <ersul4ik@mail.ru>
2017-05-08 12:30:54 +0600

    Created and described the application profile and infrastructure

--- applications/profiles/models.py
+++ applications/profiles/models.py
@@ -14,17 +14,22 @@ GENDER_CHOICES = (
 
 class Profile(models.Model):
     user = models.ForeignKey(User, verbose_name='Пользователь')
-    first_name = models.CharField(max_length=15)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=15, verbose_name='Имя')
+    last_name = models.CharField(max_length=30, verbose_name='Фамилия')
     position = models.ForeignKey(Position, verbose_name='Должность')
-    departament = models.ForeignKey(Departament, verbose_name='Отдел')
-    leader = models.BooleanField()
     gender = models.CharField(max_length=2, verbose_name='Пол', choices=GENDER_CHOICES)
-    phone = models.CharField(max_length=15)
-    address = models.CharField(max_length=50)
-    image = models.ImageField()
-    age = models.IntegerField()
+    phone = models.CharField(max_length=15, verbose_name='Телефон')
+    home_phone = models.CharField(max_length=15, verbose_name='Домашний телефон', blank=True, null=False)
+    address = models.CharField(max_length=50, verbose_name='Адрес')
+    image = models.ImageField(blank=True, null=True, verbose_name='Фотография')
+    birthday = models.DateField(verbose_name='Дата рождения')
+    active = models.BooleanField(verbose_name='Активен', default=False)
 
     def __unicode__(self):
-        return self.last_name
+        return '{} {}'.format(self.last_name, self.first_name)
 
+
+class WorkPeriod(models.Model):
+    employed = models.DateField(verbose_name='Принят на работу с ', blank=True, null=True)
+    dismissed = models.DateField(verbose_name='Уволен с ', blank=True, null=True)
+    reason_for_leaving = models.TextField(blank=True, null=True, verbose_name='Причина увольнения')

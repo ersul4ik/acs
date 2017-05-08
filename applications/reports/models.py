46d81beb38a490a2b295005161a345f0d35e5b7b
Alexander Astashov <alexander.astashov.vl@gmail.com>
2017-05-08 12:49:13 +0600

    Merge branch 'master' of /home/aastashov/workspace/acs with conflicts.

--- applications/reports/models.py
+++ applications/reports/models.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Pass(models.Model):
+    user = models.OneToOneField(User)
+    coming = models.DateTimeField(verbose_name='Пришел в', auto_now_add=True)
+    leaving = models.DateTimeField(verbose_name='Ушел в', blank=True, null=True)
+    date = models.DateField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.user.profile.get_full_name()

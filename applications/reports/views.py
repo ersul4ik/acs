e319ea5c696ca7c28c07d16f96924aa8a22b0eaf
ersul4ik <ersul4ik@mail.ru>
2017-05-19 13:50:29 +0600

    Сделал поиск и тд

--- applications/reports/views.py
+++ applications/reports/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
@@ -14,6 +14,13 @@ def report_main(request):
     template = 'main_report.html'
     if 'header_search' in request.GET:
         access_list = AccountingAccess.objects.filter(user__profile__first_name__contains=request.GET.get('username'))
+    elif 'form_search' in request.GET:
+        today = datetime.today()
+        first_date = request.GET.get('first_date') or datetime.strftime(today, '%Y-%m-01')
+        last_date = request.GET.get('last_date')
+        last_date = today if not last_date else datetime.strptime(last_date, '%Y-%m-%d')
+        last_date += timedelta(days=1)
+        access_list = AccountingAccess.objects.filter(date__range=[first_date, last_date])
     else:
         access_list = AccountingAccess.objects.filter(date=datetime.today())
     return render(request, template, {'user_list': access_list})
